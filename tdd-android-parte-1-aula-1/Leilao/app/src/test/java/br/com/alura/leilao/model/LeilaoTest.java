cd2038f55484a0407718afd3b21698d4b412ebee
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-08-26 16:18:55 -0400

    First Commit

---
+++ tdd-android-parte-1-aula-1/Leilao/app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -0,0 +1,30 @@
+package br.com.alura.leilao.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LeilaoTest {
+
+    @Test
+    public void getDescricao() {
+        // criar o cenário de teste
+        Leilao console = new Leilao("Console");
+        //executar a ação esperada
+        String descricaoDevolvida = console.getDescricao();
+
+        //testar resultado esperado
+        assertEquals("Console", descricaoDevolvida);
+    }
+
+    @Test
+    public void getMaiorLance(){
+        Leilao lance = new Leilao("lance");
+        lance.propoe(new Lance(new Usuario("Alex"), 200.0));
+
+        double maiorLanceDevolvido = lance.getMaiorLance();
+
+        assertEquals(200.0, maiorLanceDevolvido, 0.0001);
+
+    }
+}

8b978d4536478233260764dc56c01a1203f42af8
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-08-26 17:34:21 -0400

    02. Introdução aos testes automatizados

--- tdd-android-parte-1-aula-1/Leilao/app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ tdd-android-parte-1-aula-1/Leilao/app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -5,10 +5,12 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class LeilaoTest {
+    //template para o nome do teste
+    // [nome do método] [Estado de teste] [resultado esperado]
 
     //01. Implementando primeiras features
     @Test
-    public void getDescricao() {
+    public void getDescricao_QuandoRecebeDescricao_DevolveDescricao() {
         // criar o cenário de teste
         Leilao console = new Leilao("Console");
         //executar a ação esperada
@@ -20,13 +22,37 @@ public class LeilaoTest {
 
     //02. Introdução aos testes automatizados
     @Test
-    public void getMaiorLance(){
+    public void getMaiorLance_QuandoRecebeApenasUmLance_DevolveMaiorLance() {
         Leilao lance = new Leilao("lance");
         lance.propoe(new Lance(new Usuario("Alex"), 200.0));
 
-        double maiorLanceDevolvido = lance.getMaiorLance();
+        double maiorLanceDevolvidoDoConsole = lance.getMaiorLance();
+        assertEquals(200.0, maiorLanceDevolvidoDoConsole, 0.0001);
+    }
 
-        assertEquals(200.0, maiorLanceDevolvido, 0.0001);
+    @Test
+    public void getMaiorLance_QuandoRecebeMaisDeUmLanceEmOrdemCrescente_DevolveMaiorLance() {
+        //Maior Lance devolvido para o Computador
+        Leilao computador = new Leilao("Computador");
+
+        computador.propoe(new Lance(new Usuario("Alex"), 100.0));
+        computador.propoe(new Lance(new Usuario("Fran"), 200.0));
+
+        double maiorLanceDevolvidoDoComputador = computador.getMaiorLance();
+        assertEquals(200.0, maiorLanceDevolvidoDoComputador, 0.0001);
+
+    }
+
+    @Test
+    public void getMaiorLance_QuandoRecebeMaisDeUmLanceEmOrdemDecrescente_DevolveMaiorLance() {
+        //Maior Lance devolvido para o Carro
+        Leilao carro = new Leilao("Carro");
+
+        carro.propoe(new Lance(new Usuario("Alex"), 10000.0));
+        carro.propoe(new Lance(new Usuario("Fran"), 9000.0));
+
+        double maiorLanceDevolvidoCarro = carro.getMaiorLance();
+        assertEquals(10000.0, maiorLanceDevolvidoCarro, 0.0001);
 
     }
 }
